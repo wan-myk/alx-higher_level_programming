97203745a88c3441adc2d44bc70bd4755f9d4a87
wan-myk <mykle231@gmail.com>
2023-07-25 14:16:48 -0700

    Task 6 updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,28 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = 'https://jsonplaceholder.typicode.com/todos';
-
-request.get(url, { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-  });
-
-  // Printing users with completed tasks
-  for (const userId in tasksCompleted) {
-    console.log(`User ID: ${userId}, Tasks Completed: ${tasksCompleted[userId]}`);
+    });
+    console.log(completed);
   }
 });
-
